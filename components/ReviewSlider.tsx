e5d519f0e48d40f2b6661dda4c64f50c86482126
Noman Sadick <nomansadickjs@gmail.com>
2025-02-09 17:05:26 +0600

    review slider card width mobile version update

--- components/ReviewSlider.tsx
+++ components/ReviewSlider.tsx
@@ -34,8 +34,8 @@ const ReviewSlider = () => {
   };
 
   return (
-    <div className="w-full py-12 px-4">
-      <div className="w-[80%] mx-auto">
+    <div className="w-full py-12 px-2 md:px-4">
+      <div className="w-[100%] md:w-[80%] mx-auto">
         <h2 className="text-4xl md:text-5xl font-medium text-center mb-6">
           Customer <span className="text-[#146B83]">Review</span>
         </h2>
